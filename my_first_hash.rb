5bbd21c9a7332d0a1c94df38ee879dee0944908a
Anthony Chang <awchang12@gmail.com>
2018-05-06 22:03:51 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,8 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {
-  "Name" => "Anthony Chang"
-  "Age" => "26"
+  "Name" => "Anthony Chang",
   "height" => "5ft 10inches"
 }
   
